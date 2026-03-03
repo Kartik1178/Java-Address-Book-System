6748b6d6a68fc9745e3589e86e9190eb5ca48762
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-03 10:46:55 +0530

    [Kartikeya]:impleted edit contact by name

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -12,4 +12,15 @@ public class AddressBook {
     public List<Contact> getContacts() {
         return contacts;
     }
+    public void editContact(String firstName) {
+        for (Contact c : contacts) {
+            if (c.getFirstName().equalsIgnoreCase(firstName)) {
+                c.setCity("UpdatedCity");
+                System.out.println("Contact Updated");
+                return;
+            }
+        }
+        System.out.println("Contact Not Found");
+    }
+
 }
