a905312cc6746c2138fdabf5aed9c9b3946ff095
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-03 10:43:03 +0530

    [Kartikeya]: added console based contact creation

---
+++ src/AddressBookMain.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+        System.out.println("Welcome to Address Book Program");
+        System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+        System.out.print("Enter Address: ");
+        String address = sc.nextLine();
+        System.out.print("Enter City: ");
+        String city = sc.nextLine();
+        System.out.print("Enter State: ");
+        String state = sc.nextLine();
+        System.out.print("Enter Zip: ");
+        String zip = sc.nextLine();
+        System.out.print("Enter Phone: ");
+        String phone = sc.nextLine();
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
+        Contact contact = new Contact(firstName, lastName, address,
+                city, state, zip, phone, email);
+        addressBook.addContact(contact);
+        System.out.println("Contact Added Successfully!");
+    }
+}
